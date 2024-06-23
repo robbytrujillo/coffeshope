00c1800e39588764a9605b625863176e311ee7c9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-23 13:54:00 +0700

    introduction cofeeshope

--- src/pages/Splashscreen/index.js
+++ src/pages/Splashscreen/index.js
@@ -7,17 +7,17 @@ const Splashscreen = () => {
   return (
     <View style={{ flex: 1 }}>
         <ImageBackground source={Backgrounds} style={{ flex: 1 }}>
-          <View style={{ marginTop: 90 }}>
+          <View style={{ marginTop: 60 }}>
             <Image source={Splashs} />
           </View>
           <View style={{ marginTop: 30 }}>
-            <Text style={{ fontSize: 24, fontWeight: '600', color: 'white', paddingHorizontal: 100, textAlign: 'center' }}>Coffe So good, your taste buds will love its</Text>
+            <Text style={{ fontSize: 24, fontWeight: '500', color: 'white', paddingHorizontal: 100, textAlign: 'center' }}>Coffe So good, your taste buds will love its</Text>
           </View>
           <View style={{ marginTop: 30 }}>
             <Text style={{ fontSize: 14, color: 'white', textAlign: 'center', paddingHorizontal: 90 }}>The best grain, the finest roas, the most powerful flavor.</Text>
           </View>
-          <TouchableOpacity activeOpacity={ 0.7 } style={{ backgroundColor: '#00512C', alignItems: 'center', paddingHorizontal: 30, paddingVertical: 15, borderRadius: 30, marginHorizontal: 60 }}>
-            <Text style={{ fontSize: 16, fontWeight: 600, color: white }}>Get started</Text>
+          <TouchableOpacity activeOpacity={ 0.7 } style={{ backgroundColor: '#00512C', alignItems: 'center', paddingHorizontal: 30, paddingVertical: 15, borderRadius: 30, marginHorizontal: 60, marginTop: 30 }}>
+            <Text style={{ fontSize: 16, fontWeight: 600, color: 'white' }}>Get started</Text>
           </TouchableOpacity>
         </ImageBackground>
     </View>
