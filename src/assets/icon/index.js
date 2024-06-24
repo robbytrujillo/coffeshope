0884c5c7117e5fcb66ba6a299a7b991e05b705e4
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-25 03:33:13 +0700

    introduction cofeeshope | home page custom

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -5,7 +5,8 @@ import Location from "./location.png";
 import Rate from "./rate.png";
 import FavoriteOn from "./favoriteon.png";
 import FavoriteOff from "./favoriteoff.png";
-import Coffe from "./coffe.png";
+import Coffee from "./coffee.png";
 import AddButton from "./add.png";
+import GreenCoffee from "./greencoffee.png";
 
-export { Notification, Search, Filter, Location, Rate, FavoriteOn, FavoriteOff, Coffe, AddButton };
+export { Notification, Search, Filter, Location, Rate, FavoriteOn, FavoriteOff, Coffee, GreenCoffee, AddButton };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { Photoprofile, Location, Notification, Search, Filter } from "../../assets/";
-import { TextInput } from "react-native-gesture-handler";
+import { Photoprofile, Location, Notification, Search, Filter, Coffee, GreenCoffee } from "../../assets/";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 // import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Home = () => {
@@ -22,17 +22,41 @@ const Home = () => {
       </View>
       {/* End TopMenu */}
 
+      <View style={{ marginHorizontal: 30, marginTop: 15 }}>
+        <Text style={{ fontWeight: 500, fontSize: 14 }}>Good Morning, Robby</Text>
+      </View>
       {/* Search */}
-      <View style={{ flexDirection: "row", backgroundColor: "white", marginHorizontal: 30, paddingHorizontal: 20, paddingVertical: 5, borderRadius: 30, justifyContent: "space-between", alignItems: "center", marginTop: 30 }}>
+      <View style={{ flexDirection: "row", backgroundColor: "white", marginHorizontal: 30, paddingHorizontal: 20, paddingVertical: 5, borderRadius: 30, justifyContent: "space-between", alignItems: "center", marginTop: 20 }}>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Image source={Search} />
-          <TextInput placeholder="Search Coffee..." style={{ marginLeft: '25'}}></TextInput>
+          <TextInput placeholder="Search Coffee..." style={{ marginLeft: "25" }}></TextInput>
         </View>
         <View>
           <Image source={Filter} />
         </View>
       </View>
       {/* End Search */}
+
+      <View>
+        <View>
+          <Text>Categories</Text>
+        </View>
+        <ScrollView horizontal={true}>
+          {/* menu */}
+          <TouchableOpacity activeOpacity={0.7} style={{ backgroundColor: "#00582F", paddingVertical: 5, paddingHorizontal: 5, flexDirection: "row", alignItems: "center", borderRadius: 30 }}>
+            <Image source={Coffee} />
+            <Text style={{ color: "white", marginLeft: 5 }}>Cappucino</Text>
+          </TouchableOpacity>
+          {/* end menu */}
+
+          {/* menu */}
+          <TouchableOpacity activeOpacity={0.7} style={{ backgroundColor: "white", paddingVertical: 5, paddingHorizontal: 5, flexDirection: "row", alignItems: "center", borderRadius: 30, elevation: 2, shadowColor: "black" }}>
+            <Image source={ GreenCoffee } />
+            <Text style={{ color: "#00582F", marginLeft: 5 }}>Cappucino</Text>
+          </TouchableOpacity>
+          {/* end menu */}
+        </ScrollView>
+      </View>
     </View>
   );
 };
