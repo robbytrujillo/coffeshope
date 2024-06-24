cf2516e3a3e885727e4cd2d447ce6508d7b07633
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-24 08:40:30 +0700

    introduction cofeeshope | bottom Tabs Navigation custom

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,28 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { Photoprofile } from '../../assets/images'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { Photoprofile, Location, Notification } from "../../assets/";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Home = () => {
   return (
     <View style={{ flex: 1 }}>
-      {/* <Text>Home</Text> */}
-      <View>
-        <View>
+      {/* TopMenu */}
+      <View style={{ flexDirection: "row", marginTop: 30, marginHorizontal: 30, justifyContent: "space-between", alignItems: "center" }}>
+        <TouchableOpacity activeOpacity={0.7}>
           <Image source={Photoprofile} />
-        </View>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image source={Location} />
+          <Text style={{ fontWeight: 500, fontSize: 12, marginLeft: 5 }}>Jakarta, Indonesia</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image source={Notification} />
+        </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
